af5024e3cfb6dfc08365714c82bdc1da9686bd6c
Sushant <sushant9322@gmail.com>
2021-04-16 20:36:48 +0530

    Final Commit

--- frontend/src/app/components/home/home.component.spec.ts
+++ frontend/src/app/components/home/home.component.spec.ts
@@ -1,9 +1,11 @@
+import { HttpClient,HttpResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { NgModule } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { doesNotReject } from 'node:assert';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable } from 'rxjs/internal/Observable';
 import { RecipeDTO } from 'src/app/model/RecipeDTO';
 import { ApiService } from 'src/app/service/api.service';
 import { environment } from 'src/environments/environment';
@@ -15,15 +17,17 @@ describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
   let httpMock: HttpTestingController;
   let apiService: ApiService;
+  let httpClient:HttpClient;
   let recipeDTO:RecipeDTO;
   beforeEach(() => {
+    HomeComponent.prototype.ngOnInit=()=>{};
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule,MatDialogModule,MatSnackBarModule ],
+      imports: [HttpClientTestingModule,MatDialogModule,MatSnackBarModule,NoopAnimationsModule ],
       providers: [{provide:ApiService},
         { provide: MatDialogRef, useValue: {} },
         { provide: MAT_DIALOG_DATA, useValue: { myData: null } }
     ],
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent]
     })
     .compileComponents();
   });
@@ -32,29 +36,64 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpClient = TestBed.inject(HttpClient);
     apiService = TestBed.inject(ApiService);
     httpMock = TestBed.inject(HttpTestingController);
     recipeDTO=new RecipeDTO();
-  });
-
-  it('get recipe should give one recipe',()=>{
-    recipeDTO.id=1;
+    
     recipeDTO.created=new Date();
     recipeDTO.name="Dish";
     recipeDTO.servings=2;
     recipeDTO.category=true;
     recipeDTO.ingredients=["Oil"];
     recipeDTO.instructions="Just pour oil on pan";
-    apiService.getRecipe(1).subscribe((res)=>{
-      expect(res).toEqual(recipeDTO);
-      
-    });
-
-    // const req = httpMock.expectOne({ method: 'GET', url: environment.baseURL + "recipes/1" });
-    // expect(req.request.method).toEqual("GET");
-    // req[0].flush(recipeDTO);
-    // httpMock.verify();
+    httpMock.verify({ignoreCancelled:true});
+  });
+  afterEach(() => {
+    
+    httpMock.verify({ignoreCancelled:true}); //Verifies that no requests are outstanding.
+  });
+  it('get recipe should give one recipe',()=>{
+    component.showRecipe(1);
+    const req = httpMock.expectOne(req=>req.method==='GET' && req.url===environment.baseURL + 'recipes/1');
+    expect(req.request.method).toEqual("GET");
+    req.flush(recipeDTO);
+    
+  });
   
+  it('get recipe should give all recipes',()=>{
+    recipeDTO.id=1;
+    var recipes:RecipeDTO[]=new Array;
+    recipes.push(recipeDTO);
+    component.getRecipesList();
+    const req = httpMock.expectOne({ method: 'GET', url: environment.baseURL + "recipes" });
+    expect(req.request.method).toEqual("GET");
+    req.flush(recipes);
+    
+  
+  });
+  // it('Save recipe should save one recipe',()=>{
+    
+  //   delete recipeDTO.id;
+    
+  //   component.saveOrUpdateRecipe(recipeDTO);
+  //   expect(1).toEqual(1);
+  //   //spyOn(component,"getRecipesList").and.callFake(function(){return null;});
+  //   const req = httpMock.expectOne(req=>req.method==='POST' && req.url===environment.baseURL + 'recipes/');
+  //   expect(req.request.method).toEqual("POST");
+  //   req.flush(recipeDTO);
+  // });
+  
+  it('delete recipe should delete one recipe',()=>{
+    spyOn(component,"getRecipesList").and.callFake(function(){return null;});
+    component.deleteRecipe(1);
+    //const reqs=httpMock.match(environment.baseURL + 'recipes');
+    const req = httpMock.expectOne(req=>req.method==='DELETE' && req.url===environment.baseURL + 'recipes/1');
+    
+    req.flush({});
+  });
+  it('should create', () => {
+    component.openDialog(3,null);
   });
 
   it('should create', () => {
@@ -62,10 +101,4 @@ describe('HomeComponent', () => {
   });
 });
 
-@NgModule({
-  imports: [MatDialogModule],
-  entryComponents: [
-    
-  ],
-})
-class HomeComponentTestModule { }
+
--- frontend/src/app/service/api.service.spec.ts
+++ frontend/src/app/service/api.service.spec.ts
@@ -1,24 +1,115 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ApiService } from './api.service';
+import { RecipeDTO } from '../model/RecipeDTO';
+import { environment } from 'src/environments/environment';
+import { Recipe } from '../model/recipe.model';
 
 TestBed.configureTestingModule({
   imports: [HttpClientTestingModule],
   providers: []
 });
+
 describe('ApiService', () => {
   let service: ApiService;
-
+  let httpMock: HttpTestingController;
+  let apiService: ApiService;
+  let httpClient: HttpClient;
+  let recipeDTO: RecipeDTO;
+  let recipe:Recipe;
   beforeEach(() => {
+    
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: []
+      providers: [ApiService]
     });
     service = TestBed.inject(ApiService);
+    httpClient = TestBed.inject(HttpClient);
+    apiService = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+    recipeDTO = new RecipeDTO();
+    recipe = new Recipe();
+    recipeDTO.id = 1;
+    recipeDTO.created = new Date();
+    recipeDTO.name = "Dish";
+    recipeDTO.servings = 2;
+    recipeDTO.category = true;
+    recipeDTO.ingredients = ["Oil"];
+    recipeDTO.instructions = "Just pour oil on pan";
+
+    recipe.name = "Dish";
+    recipe.servings = 2;
+    recipe.category = true;
+    recipe.ingredients = "Oil";
+    recipe.instructions = "Just pour oil on pan";
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  afterEach(() => {
+    httpMock.verify(); //Verifies that no requests are outstanding.
+  });
+  
+  it('get recipe should give one recipe', () => {
+    
+    apiService.getRecipe(1).subscribe((res) => {
+      expect(res).toEqual(recipeDTO);
+
+    });
+    const req = httpMock.expectOne(req => req.method === 'GET' && req.url === environment.baseURL + 'recipes/1');
+    expect(req.request.method).toEqual("GET");
+    req.flush(recipeDTO);
+    httpMock.verify();
+    // const req = httpMock.expectOne({ method: 'GET', url: environment.baseURL + "recipes/1" });
+    //const req = httpMock.match({ method: 'GET', url: environment.baseURL + "recipes/1" });
+  });
+
+  it('get recipe should give all recipes', () => {
+    var recipes: RecipeDTO[] = new Array;
+    recipes.push(recipeDTO);
+    apiService.getRecipes().subscribe((res) => {
+      expect(res).toEqual(recipes);
+
+    });
+
+    const req = httpMock.expectOne({ method: 'GET', url: environment.baseURL + "recipes" });
+    expect(req.request.method).toEqual("GET");
+    req.flush(recipes);
+    httpMock.verify();
+
+  });
+
+  it('delete recipe should delete one recipe',()=>{
+    apiService.deleteRecipe(1).subscribe((res) => {
+      expect(res).toEqual("");
+
+    });
+    const req = httpMock.expectOne(req=>req.method==='DELETE' && req.url===environment.baseURL + 'recipes/1');
+    expect(req.request.method).toEqual("DELETE");
+    //req.flush();
+    httpMock.verify();
+  });
+  it('Add recipe should add one recipe',()=>{
+    apiService.saveRecipe(recipe).subscribe((res) => {
+      expect(res).toEqual("");
+
+    });
+    const req = httpMock.expectOne(req=>req.method==='POST' && req.url===environment.baseURL + 'recipes');
+    expect(req.request.method).toEqual("POST");
+    //req.flush();
+    httpMock.verify();
+  });
+  
+  it('Update recipe should add update recipe',()=>{
+    apiService.updateRecipe(recipe).subscribe((res) => {
+      expect(res).toEqual("");
+
+    });
+    const req = httpMock.expectOne(req=>req.method==='PUT' && req.url===environment.baseURL + 'recipes');
+    expect(req.request.method).toEqual("PUT");
+    //req.flush();
+    httpMock.verify();
   });
 });
